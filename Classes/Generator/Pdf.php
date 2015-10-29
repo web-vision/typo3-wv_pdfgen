86cc48ae6c616c29bfbf4cb272d293d4abe7046f
Daniel Siepmann <d.siepmann@web-vision.de>
2015-10-29 10:13:23 +0100

    [CLEANUP] For sonar and phpcs
    * Initial cleanup after first indexing

--- Classes/Generator/Pdf.php
+++ Classes/Generator/Pdf.php
@@ -1,31 +1,18 @@
 <?php
 namespace WebVision\WvPdfgen\Generator;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2015 Daniel Siepmann <d.siepmann@web-vision.de>, web-vision GmbH
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the text file GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
- *
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\HttpUtility;
@@ -35,8 +22,8 @@ use TYPO3\CMS\Core\Utility\HttpUtility;
  *
  * @author Daniel Siepmann <d.siepmann@web-vision.de>
  */
-class Pdf {
-
+class Pdf
+{
     /**
      * The configuration passed by TS.
      * @var array
@@ -47,14 +34,16 @@ class Pdf {
      * Userfunction for TYPO3.
      *
      * Will generate the PDF for the current url and take $conf into account.
+     *
      * @see readme.md
      *
      * @param string $content API by TYPO3, ignored by this function.
      * @param array $conf The configuration for this script. @see readme.md
      *
-     * @return void Will create an HTTP redirect instead.
+     * @return void
      */
-    public function main( $content, array $conf ) {
+    public function main($content, array $conf)
+    {
         $this->configuration = $conf;
 
         // Generate PDF
@@ -72,14 +61,15 @@ class Pdf {
      *
      * @return WebVision\WvPdfgen\Generator\Pdf The current instance for chaining.
      */
-    protected function generatePdf() {
+    protected function generatePdf()
+    {
         // Build command
-        $command = escapeshellcmd( $this->configuration['binary'] )
-            . '  ' . escapeshellarg( $this->getUrlForGeneration() )
-            . '  ' . escapeshellarg( $this->getFileName() );
+        $command = escapeshellcmd($this->configuration['binary'])
+            . '  ' . escapeshellarg($this->getUrlForGeneration())
+            . '  ' . escapeshellarg($this->getFileName());
 
         // Execute command
-        exec( $command );
+        exec($command);
 
         return $this;
     }
@@ -89,13 +79,14 @@ class Pdf {
      *
      * @return string The full absolute path to the file.
      */
-    protected function getFileName() {
+    protected function getFileName()
+    {
         // Define / Generate paths
         $folderPath = PATH_site . 'typo3temp/gen_pdfs/';
-        $fileName = md5( $this->getUrlForGeneration() ) . '.pdf';
+        $fileName = md5($this->getUrlForGeneration()) . '.pdf';
 
         // Create folder if it doesn't exist yet. (=> TEMP folder)
-        GeneralUtility::mkdir_deep( $folderPath );
+        GeneralUtility::mkdir_deep($folderPath);
 
         return $folderPath . $fileName;
     }
@@ -107,8 +98,9 @@ class Pdf {
      *
      * @return string The url.
      */
-    protected function getPdfUrl() {
-        return $this->getDomain() . str_replace( PATH_site, '', $this->getFileName() );
+    protected function getPdfUrl()
+    {
+        return $this->getDomain() . str_replace(PATH_site, '', $this->getFileName());
     }
 
     /**
@@ -118,15 +110,15 @@ class Pdf {
      *
      * @return string The url
      */
-    protected function getUrlForGeneration() {
+    protected function getUrlForGeneration()
+    {
         $urlToConvert = $this->getDomain() . $GLOBALS['TSFE']->siteScript;
 
         // Remove type parameter to generate the real url, not the PDF (= endless loop)
-        $urlToConvert = str_ireplace( 'type=' . $this->configuration['typeNum'], '', $urlToConvert );
-        // Remove url extension like ".pdf" as it's realurl rewriting the type.
-        $urlToConvert = str_ireplace( $this->configuration['urlExtension'], '', $urlToConvert );
+        $urlToConvert = str_ireplace('type=' . $this->configuration['typeNum'], '', $urlToConvert);
 
-        return $urlToConvert;
+        // Remove url extension like ".pdf" as it's realurl rewriting the type.
+        return str_ireplace($this->configuration['urlExtension'], '', $urlToConvert);
     }
 
     /**
@@ -134,8 +126,8 @@ class Pdf {
      *
      * @return string
      */
-    protected function getDomain() {
-        return GeneralUtility::getIndpEnv( 'TYPO3_SITE_URL' );
+    protected function getDomain()
+    {
+        return GeneralUtility::getIndpEnv('TYPO3_SITE_URL');
     }
-
 }
