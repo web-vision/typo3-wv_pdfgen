5b2393aea459b5f1337b462ff9eee1e764d44b30
J. Moroni <j.moroni@web-vision.de>
2015-10-30 18:23:28 +0100

    [FEATURE] Added possibility to add cli parameters
    * Wkhtmltopdf needs cli parameters to be configured

--- Classes/Generator/Pdf.php
+++ Classes/Generator/Pdf.php
@@ -16,6 +16,7 @@ namespace WebVision\WvPdfgen\Generator;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\HttpUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /**
  * This class will generate PDFs based of the current URL.
@@ -31,9 +32,16 @@ class Pdf
     protected $configuration = array();
 
     /**
-     * Userfunction for TYPO3.
+     * The ContentObjectRenderer from TYPO3 which is added automatically.
+     * @var ContentObjectRenderer
+     */
+    public $cObj;
+
+    /**
+     * Userfunction for TYPO3. Which will generate the PDF for the current url
+     * and take $conf into account.
      *
-     * Will generate the PDF for the current url and take $conf into account.
+     * Will create an HTTP redirect instead pointing to the generated PDF.
      *
      * @see readme.md
      *
@@ -44,7 +52,7 @@ class Pdf
      */
     public function main($content, array $conf)
     {
-        $this->configuration = $conf;
+        $this->processConfiguration($conf);
 
         // Generate PDF
         $this->generatePdf();
@@ -59,16 +67,16 @@ class Pdf
     /**
      * Generate the PDF and persist it in Filesystem
      *
-     * @return WebVision\WvPdfgen\Generator\Pdf The current instance for chaining.
+     * @return Pdf
      */
     protected function generatePdf()
     {
         // Build command
         $command = escapeshellcmd($this->configuration['binary']) .
+            $this->getCliParameter() .
             ' ' . escapeshellarg($this->getUrlForGeneration()) .
             ' ' . escapeshellarg($this->getFileName());
 
-        // Execute command
         exec($command);
 
         return $this;
@@ -119,6 +127,65 @@ class Pdf
 
         // Remove url extension like ".pdf" as it's realurl rewriting the type.
         return str_ireplace($this->configuration['urlExtension'], '', $urlToConvert);
+    }
+
+    /**
+     * Get cli parameter to include while PDF generation.
+     *
+     * @return string
+     */
+    protected function getCliParameter()
+    {
+        $cliParameter = '';
+
+        foreach ($this->configuration['cliparameter'] as $key => $value) {
+            $cliParameter .= ' --' . $key . ' ' . $value;
+        }
+
+        return $cliParameter;
+    }
+
+    /**
+     * Will parse the given configuration and save the processed configuration.
+     *
+     * Empty options will be ignored.
+     *
+     * @param array $configuration The original configuration passed in.
+     *
+     * @return PDF
+     */
+    protected function processConfiguration(array $configuration)
+    {
+        $configuration = array_filter($configuration);
+
+        foreach ($configuration as $key => $value) {
+            if(strpos($key, '.')) {
+                continue;
+            }
+
+            $this->configuration[$key] = $value;
+        }
+
+        $configuration = $configuration['cliparameter.'];
+
+        foreach($configuration as $key => $value) {
+            // Don't process sub array with further configuration, this is done
+            // by stdWrap
+            if(strpos($key, '.') === (strlen($key) - 1)) {
+                continue;
+            }
+
+            $this->configuration['cliparameter'][$key] = $value;
+
+            // Process stdWrap if sub array exists.
+            if(isset($configuration[$key . '.']) && is_array($configuration[$key . '.'])) {
+                $this->configuration['cliparameter'][$key] = trim(
+                    $this->cObj->stdWrap($configuration[$key], $configuration[$key . '.'])
+                );
+            }
+        }
+
+        return $this;
     }
 
     /**
